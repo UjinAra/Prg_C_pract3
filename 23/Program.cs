74c4fe8cc65db7a93db91c45ef39f53de6d92b03
UjinAra <aev_co@mail.ru>
2022-11-26 22:34:34 +0700

    Пример 23

---
+++ 23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число N:");
+int N = Convert.ToInt32(ReadLine());
+
+double result = 1;
+
+if (N >= 1)
+{
+    for (int i=1 ; i<= N; i++)
+    {
+    result = Math.Pow (i,3);
+    Write($" {result} ");
+    }
+}
+ else  
+ {
+    Write(" Не корректный ввод ");
+ }
